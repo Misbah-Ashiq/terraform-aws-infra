d9eae9d333fdfaf6f3473c9a5f228733d3585ff0
misbah-ashiq <misbahdevops46@gmail.com>
2025-09-01 21:23:23 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,45 @@
-variable "project_name"      { type = string  default = "iac-ec2-vpc" }
-variable "aws_region"        { type = string  default = "ap-south-1" }
+variable "project_name" {
+  type    = string
+  default = "terraform-aws-infra"
+}
 
-variable "vpc_cidr"          { type = string  default = "10.0.0.0/16" }
-variable "public_subnet_cidr"{ type = string  default = "10.0.1.0/24" }
-variable "private_subnet_cidr"{ type = string default = "10.0.2.0/24" }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
 
-# Restrict SSH/HTTP to your IP for security; for demo you can use 0.0.0.0/0
-variable "allowed_ssh_cidr"  { type = string  default = "0.0.0.0/0" }
-variable "allowed_http_cidr" { type = string  default = "0.0.0.0/0" }
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
 
-variable "instance_type"     { type = string  default = "t3.micro" }
+variable "public_subnet_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
 
-# You can pass an existing EC2 key pair name or create/import later
-variable "key_pair_name"     { type = string  default = "" }
+variable "private_subnet_cidr" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+
+variable "allowed_ssh_cidr" {
+  type    = string
+  default = "204.236.206.26/32"
+}
+
+variable "allowed_http_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "key_pair_name" {
+  type    = string
+  default = "keylogin"
+}
+
